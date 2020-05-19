70874ce11d17b47f93708ee4b461cc53f50d1850
jasonmcgraw <jasonm654@gmail.com>
2020-05-19 08:57:08 -0400

    Spelling

--- Todo/App.xaml.cs
+++ Todo/App.xaml.cs
@@ -8,7 +8,7 @@ namespace Todo
         //Some notes
          //- Didn't use an IoC container for brevity but did inject a dependency
          //- Didn't opt for MVVM helper like prism or something to keep it simple
-         //- Edit and delete items using contect actions(swipe right iOS / hold down Android)
+         //- Edit and delete items using context actions(swipe left iOS / hold down Android)
          //- Used synchronous db calls for simplicity, I understand that larger data sets and longer load times
          //  would benefit from asynchronous calls, and would probably call for a UI loading indicator
          //- Most of the TodoItems stuff is just inherited from Todo
